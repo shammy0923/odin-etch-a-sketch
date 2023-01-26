279d9dee43f500a73562c2afa5017432d28b16bb
shammy0923 <roseshammy@gmail.com>
2023-01-25 19:31:47 -0500

    Implemented  "Clear" functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Document Element Variables
 
 const sketchGrid = document.getElementById("sketch-grid");
+const clearButton = document.getElementById("clear-button");
 
 // Setup the Etch-a-sketch grid
 
@@ -19,8 +20,19 @@ function makeRows(rows, cols) {
         divBlock.addEventListener("mouseover", function() {
             divBlock.classList.remove("div-block");
             divBlock.classList.add("div-block-hovered");
-        })
+        });
     }
 }
 
+// Clear Functionality
+
+clearButton.addEventListener("click", function() {
+    let divElement = document.querySelectorAll(".div-block-hovered");
+    for (var i = 0; i < divElement.length; i++) {
+        divElement[i].classList.remove("div-block-hovered");
+        divElement[i].classList.add("div-block");
+    }
+});
+
+// Creating grid
 makeRows(16, 16);
